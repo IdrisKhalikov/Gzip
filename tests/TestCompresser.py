3794e4d352c702ff06020a910bfb77a8ab81b9f9
NeonyFenyx <idrishal277@gmail.com>
2023-06-03 16:40:38 +0500

    Added all tests

---
+++ tests/TestCompresser.py
@@ -0,0 +1,18 @@
+import unittest
+import random
+import pygzip
+import os
+
+class TestCompresser(unittest.TestCase):
+
+    def test_compress_and_decompress(self):
+        text = random.randbytes(10000)
+        with open('test.txt', 'wb') as f:
+            f.write(text)
+        pygzip.compress('test.txt')
+        os.remove('test.txt')
+        pygzip.decompress('test.gz')
+        actual = bytearray()
+        with open('test.txt', 'rb') as f:
+            actual.extend(f.read())
+        self.assertSequenceEqual(text, actual)
